61dcfbfb6f20e29aff1741f8b136de320d6dfeca
Thomas Prusher <tprusher@hotmail.co.uk>
2020-08-24 22:08:20 +1200

    Read Inventory & Post to RDS git push

--- OrderStock.py
+++ OrderStock.py
@@ -1,22 +1,33 @@
 
 
 """
-Output order:
-1. Required stock level
-2. Supplier ID
-3. ProductKey
-4. Date of Order
-5. 
-
+Goal: 
+1. Will need to query RDS MySQL DB -- COMPLETE
+2. Perform Calculations off the back of that to determine whether or not a PO should be raised.. 
 """
 
 
-def my_order(value):
-    x = value
-    print(x)
-    my_order(x - 1)
+import pymysql
+import pandas as pd
 
+connection = pymysql.connect(
+    host = 'autostockordering.co7conkey36s.us-east-1.rds.amazonaws.com',
+    port = 3306,
+    user = 'adminTom',
+    password = 'XXHnW0lpK1jKaDujJaQA',
+    database = "Supplier"
+)
 
+cur = connection.cursor()
 
+# Goal 1 (Get Data): 
+def get_data():
+    sql = """select * from stock;"""
+    print("<< RUNNING SQL:\n%s\n>>"%(sql))
+    table_data = pd.read_sql(sql, con=connection)   
 
-my_order(10)
+    print("<< TABLE DATA >>")
+    print(table_data)
+
+    
+
