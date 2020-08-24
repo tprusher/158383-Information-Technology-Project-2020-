8d710ef20f3a92c5c899385309f93811f7a3a3af
Thomas Prusher <tprusher@hotmail.co.uk>
2020-08-24 20:30:32 +1200

    initial push

---
+++ OrderStock.py
@@ -0,0 +1,22 @@
+
+
+"""
+Output order:
+1. Required stock level
+2. Supplier ID
+3. ProductKey
+4. Date of Order
+5. 
+
+"""
+
+
+def my_order(value):
+    x = value
+    print(x)
+    my_order(x - 1)
+
+
+
+
+my_order(10)
